91e2fbe93a0136b90bccfdf76d1d197e580a5771
Sepideh Azizi <sepid.azizi97@gmail.com>
2026-08-07 12:36:37 -0700

    Refactor SQL query for ridership trends

--- Ridership_Trends.sql
+++ Ridership_Trends.sql
@@ -1,112 +1,68 @@
 SELECT
-    apc."Year",
-    apc."Month Number",
-    apc."Month",
-    apc."Year-Month",
+    CAST(dd.Year AS VARCHAR(4)) AS "Year",
+    dd.Month AS "Month Number",
+    dd.MonthName AS "Month",
 
-    apc."Route Short Name",
-    apc."Route Name",
+    CAST(dd.Year AS VARCHAR(4))
+        + '-'
+        + RIGHT('0' + CAST(dd.Month AS VARCHAR(2)), 2)
+        AS "Year-Month",
 
-    apc."APC Ridership",
-    ISNULL(fare."Fare Count Ridership", 0) AS "Fare Count Ridership",
+    r.RouteShortName AS "Route Short Name",
+    r.RouteName AS "Route Name",
 
-    apc."APC Ridership" - ISNULL(fare."Fare Count Ridership", 0)
-        AS "Difference APC Minus Fare",
+    SUM(fc.FareCount) AS "Total Fare Counts"
 
-    CASE
-        WHEN apc."APC Ridership" = 0 THEN NULL
-        ELSE ROUND(
-            100.0 * ISNULL(fare."Fare Count Ridership", 0) / apc."APC Ridership",
-            1
-        )
-    END AS "Fare Count as % of APC"
-
-FROM (
+FROM
+(
     SELECT
-        CAST(dd.Year AS VARCHAR(4)) AS "Year",
-        dd.Month AS "Month Number",
-        dd.MonthName AS "Month",
-        CAST(dd.Year AS VARCHAR(4)) + '-' + RIGHT('0' + CAST(dd.Month AS VARCHAR(2)), 2) AS "Year-Month",
-
-        r.RouteShortName AS "Route Short Name",
-        r.RouteName AS "Route Name",
-
-        SUM(tp.BoardCount) AS "APC Ridership"
-
-    FROM VehicleLocationTP tp
-
-    INNER JOIN DateDimension dd
-        ON tp.ActualArriveDateKey = dd.DateDimensionKey
-
-    INNER JOIN sch_Route r
-        ON tp.RouteKey = r.RouteKey
-
-    WHERE
-        dd.FullDate >= CAST('2023-01-01' AS DATETIME)
-        AND dd.FullDate < DATEADD(day, 1, CAST(GETDATE() AS DATE))
-
-        AND tp.BoardCount IS NOT NULL
-
-        AND r.RouteShortName IN (
-            '1','10','123','123s','170','2','20','225','240',
-            '25','26','26s','27','3','40','41','42',
-            '47','48','50','64','65','67','68'
-        )
-
-    GROUP BY
-        CAST(dd.Year AS VARCHAR(4)),
-        dd.Month,
-        dd.MonthName,
-        CAST(dd.Year AS VARCHAR(4)) + '-' + RIGHT('0' + CAST(dd.Month AS VARCHAR(2)), 2),
-        r.RouteShortName,
-        r.RouteName
-) apc
-
-LEFT JOIN (
-    SELECT
-        CAST(dd.Year AS VARCHAR(4)) AS "Year",
-        dd.Month AS "Month Number",
-        dd.MonthName AS "Month",
-        CAST(dd.Year AS VARCHAR(4)) + '-' + RIGHT('0' + CAST(dd.Month AS VARCHAR(2)), 2) AS "Year-Month",
-
-        r.RouteShortName AS "Route Short Name",
-        r.RouteName AS "Route Name",
-
-        SUM(vf.FareCount) AS "Fare Count Ridership"
+        vf.EventDateKey,
+        vf.WorkItemCompletedKey,
+        vf.FareTypeKey,
+        MAX(vf.FareCount) AS FareCount
 
     FROM VehicleLocationTPFare vf
 
-    INNER JOIN DateDimension dd
-        ON dd.DateDimensionKey = vf.EventDateKey
-
-    INNER JOIN sch_WorkItemCompleted wic
-        ON wic.WorkItemCompletedKey = vf.WorkItemCompletedKey
-
-    INNER JOIN sch_Route r
-        ON r.RouteKey = wic.RouteKey
-
     WHERE
-        dd.FullDate >= CAST('2023-01-01' AS DATETIME)
-        AND dd.FullDate < DATEADD(day, 1, CAST(GETDATE() AS DATE))
-
-        AND vf.FareTypeKey NOT IN (9,11,14,20)
-
-        AND vf.FareCount IS NOT NULL
-
-        AND r.RouteShortName IN (
-            '1','10','123','123s','170','2','20','225','240',
-            '25','26','26s','27','3','40','41','42',
-            '47','48','50','64','65','67','68'
-        )
+        vf.FareCount IS NOT NULL
+        AND vf.FareTypeKey NOT IN (9, 11, 14, 20)
 
     GROUP BY
-        CAST(dd.Year AS VARCHAR(4)),
-        dd.Month,
-        dd.MonthName,
-        CAST(dd.Year AS VARCHAR(4)) + '-' + RIGHT('0' + CAST(dd.Month AS VARCHAR(2)), 2),
-        r.RouteShortName,
-        r.RouteName
-) fare
+        vf.EventDateKey,
+        vf.WorkItemCompletedKey,
+        vf.FareTypeKey
+) fc
 
-ON apc."Year-Month" = fare."Year-Month"
-AND apc."Route Short Name" = fare."Route Short Name"
+INNER JOIN DateDimension dd
+    ON dd.DateDimensionKey = fc.EventDateKey
+
+INNER JOIN sch_WorkItemCompleted wic
+    ON wic.WorkItemCompletedKey = fc.WorkItemCompletedKey
+
+INNER JOIN sch_Route r
+    ON r.RouteKey = wic.RouteKey
+
+WHERE
+    dd.FullDate >= CAST('2023-01-01' AS DATETIME)
+
+    AND dd.FullDate < DATEADD(
+        day,
+        1,
+        CAST(GETDATE() AS DATE)
+    )
+
+    AND r.RouteShortName IN
+    (
+        '1', '10', '123', '123s', '170',
+        '2', '20', '225', '240', '25',
+        '26', '26s', '27', '3', '40',
+        '41', '42', '47', '48', '50',
+        '64', '65', '67', '68'
+    )
+
+GROUP BY
+    dd.Year,
+    dd.Month,
+    dd.MonthName,
+    r.RouteShortName,
+    r.RouteName
